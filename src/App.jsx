500d6403be20f00d63c7279f03e34434f846d0d5
leoleoyuuki <rm550373@fiap.com.br>
2023-08-22 09:54:06 -0300

    Componentes React

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import Cabecalho from "./components/Cabecalho";
 
 export default function App() {
   return (
     <>
 
-      <h1>First deploy into VERCEL</h1>
-      <h2>Vite + React</h2>
-      <p>Coded By - rm550373</p>
-      <div>
-        <p>Turma - 1TDSPL</p>
-        <p>FIAP</p>
+      <Cabecalho/>
+      
+      <div className="container">
+
+        <section>
+          <h2>Vite + React</h2>
+          <p>Coded By - rm550373</p>
+        
+          <p>Turma - 1TDSPL</p>
+          <p>FIAP</p>
+        </section>
+
+        <footer>
+          <a href="#">GitHub</a>
+          <a href="#">GitHub</a>
+          <a href="#">GitHub</a>
+        </footer>
       </div>
 
     </>
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,11 @@
+
+export default function Cabecalho() {
+  return (
+    <>
+     <header>
+          <h1>First deploy into VERCEL</h1>
+        </header>
+
+    </>
+  )
+}
